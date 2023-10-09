c9028c00b935676c5e30eeafe9e976b1faa7f2e6
Аль-Асали Мохамед Русланович <mohhamedion@gmail.com>
2023-10-09 17:06:21 +0300

    init classes

---
+++ src/Classes/WhileLoop.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+class WhileLoop
+{
+
+    public $loopRunCondition = true;
+
+    public function run()
+    {
+
+        while ($this->loopRunCondition)
+        {
+
+        }
+
+    }
+}
